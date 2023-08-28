c458b55c908b519c342b09d7988af67cfed0ff5f
cip170 <charlottephilipps@gmail.com>
2023-08-28 23:13:54 +1000

    adding save data & server js code

---
+++ server.js
@@ -0,0 +1,18 @@
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+const app = express;
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server is listening at ${PORT}!`);
+});
